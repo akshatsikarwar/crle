0aa277c9fb7c465e59eb71b1b4b4a13d29457f06
Akshat Sikarwar <akshat.sikarwar@gmail.com>
2022-10-25 22:35:02 -0400

    fix print_hex

--- tohex.h
+++ tohex.h
@@ -1,17 +1,17 @@
 #ifndef INCLUDE_TOHEX_H
 #define INCLUDE_TOHEX_H
-#define print_hex(p, s)                                                                            \
-    ({                                                                                             \
-        char map[] = {'0', '1', '2', '3', '4', '5', '6', '7',                                      \
-                      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};                                     \
-        char *from = p;                                                                            \
-        char *to = p + s;                                                                          \
-        while (from < to) {                                                                        \
-            char a = (*from & 0xf0) >> 4;                                                          \
-            char b = (*from & 0x0f);                                                               \
-            printf("%c%c\n", map[a], map[b]);                                                      \
-            ++from;                                                                                \
-        }                                                                                          \
-        puts("");                                                                                  \
+#define print_hex(p, s)                                                        \
+    ({                                                                         \
+        uint8_t map[] = {'0', '1', '2', '3', '4', '5', '6', '7',               \
+                         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};              \
+        uint8_t *from = (char *)p;                                             \
+        uint8_t *to = from + s;                                                \
+        while (from < to) {                                                    \
+            uint8_t a = (*from & 0xf0) >> 4;                                   \
+            uint8_t b = (*from & 0x0f);                                        \
+            printf("%c%c", map[a], map[b]);                                    \
+            ++from;                                                            \
+        }                                                                      \
+        puts("");                                                              \
     })
 #endif //INCLUDE_TOHEX_H
